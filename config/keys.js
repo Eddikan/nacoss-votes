6b82057befee4c93afd0203cd0127288159d7f75
Eddikan <imeekwere15@gmail.com>
2020-12-28 16:44:49 +0100

    init
    initial one

---
+++ config/keys.js
@@ -0,0 +1,15 @@
+module.exports = {
+    //mongoURI: 'mongodb://localhost:27017/finalvotes',
+    mongoURI: 'mongodb+srv://eddy:0000@goat.xfo0n.mongodb.net/Goat?retryWrites=true&w=majority',
+    secret: 'Eddy is the goat',
+    pusherAppId: '951491',
+    pusherKey: '15162154cd5c887fb90a',
+    pusherSecret: '8a49754ca197383f3b3d',
+    pusherCluster: 'eu',
+    pusherEncrypted: true
+  };
+  // appId: '951491',
+  // key: '15162154cd5c887fb90a',
+  // secret: '8a49754ca197383f3b3d',
+  // cluster: 'eu',
+  // encrypted: true
